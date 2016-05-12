3bdd6ef552cab2d002f11d1f5182c7858c732053
Michal Kawalec <michal@monad.cat>
2016-05-12 20:06:08 +0200

    serve the frontend

--- src/API.hs
+++ src/API.hs
@@ -6,12 +6,15 @@ import Network.Wai (Application)
 import Database.PostgreSQL.Simple (Connection)
 
 type API = "api" :> M.MessagesAPI
+         :<|> StaticAPI
+
+type StaticAPI = Raw
 
 globalAPI :: Proxy API
 globalAPI = Proxy
 
 server :: Connection -> Server API
-server conn = M.server conn
+server conn = M.server conn :<|> serveDirectory "frontend/dist"
 
 api :: Connection -> Application
 api conn = serve globalAPI $ server conn
